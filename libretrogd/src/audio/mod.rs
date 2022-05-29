03950c8d3e7ed02b2186e6bffe17bded6bff92a5
gered <gered@blarg.ca>
2022-05-29 13:52:53 -0400

    formatting

--- libretrogd/src/audio/mod.rs
+++ libretrogd/src/audio/mod.rs
@@ -296,16 +296,24 @@ pub struct Audio {
 }
 
 impl Audio {
-    pub fn new(desired_spec: AudioSpecDesired, sdl_audio_subsystem: &AudioSubsystem) -> Result<Self, AudioError> {
+    pub fn new(
+        desired_spec: AudioSpecDesired,
+        sdl_audio_subsystem: &AudioSubsystem,
+    ) -> Result<Self, AudioError> {
         let mut spec = None;
-        let sdl_audio_device = match sdl_audio_subsystem.open_playback(None, &desired_spec, |opened_spec| {
-            let our_spec = AudioSpec::new(opened_spec.freq as u32, opened_spec.channels, opened_spec.format);
-            spec = Some(our_spec);
-            AudioDevice::new(our_spec)
-        }) {
-            Ok(audio_device) => audio_device,
-            Err(error) => return Err(AudioError::OpenDeviceFailed(error)),
-        };
+        let sdl_audio_device =
+            match sdl_audio_subsystem.open_playback(None, &desired_spec, |opened_spec| {
+                let our_spec = AudioSpec::new(
+                    opened_spec.freq as u32,
+                    opened_spec.channels,
+                    opened_spec.format,
+                );
+                spec = Some(our_spec);
+                AudioDevice::new(our_spec)
+            }) {
+                Ok(audio_device) => audio_device,
+                Err(error) => return Err(AudioError::OpenDeviceFailed(error)),
+            };
 
         if let Some(spec) = spec {
             Ok(Audio {
@@ -313,7 +321,9 @@ impl Audio {
                 sdl_audio_device,
             })
         } else {
-            Err(AudioError::OpenDeviceFailed(String::from("Device initialization failed to set AudioSpec")))
+            Err(AudioError::OpenDeviceFailed(String::from(
+                "Device initialization failed to set AudioSpec",
+            )))
         }
     }
 
