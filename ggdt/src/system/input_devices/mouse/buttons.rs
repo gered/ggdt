db60579db43b8ec524045a1ebc4f8aa3d775a639
gered <gered@blarg.ca>
2023-04-12 15:19:39 -0400

    fix mouse button event mapping
    this was preventing applications from picking up basically all non-left
    button states

--- ggdt/src/system/input_devices/mouse/buttons.rs
+++ ggdt/src/system/input_devices/mouse/buttons.rs
@@ -1,13 +1,20 @@
 use bitflags::bitflags;
 
+// equivalent to SDL's "SDL_BUTTON" macro
+#[inline]
+const fn button_mask_to_value(mask: u32) -> u32 {
+	1 << (mask - 1)
+}
+
 bitflags! {
+	#[repr(transparent)]
 	#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 	pub struct MouseButtons: u32 {
-		const LEFT_BUTTON = sdl2::mouse::MouseButton::Left as u32;
-		const MIDDLE_BUTTON = sdl2::mouse::MouseButton::Middle as u32;
-		const RIGHT_BUTTON = sdl2::mouse::MouseButton::Right as u32;
-		const X1 = sdl2::mouse::MouseButton::X1 as u32;
-		const X2 = sdl2::mouse::MouseButton::X2 as u32;
+		const LEFT_BUTTON = button_mask_to_value(sdl2::mouse::MouseButton::Left as u32);
+		const MIDDLE_BUTTON = button_mask_to_value(sdl2::mouse::MouseButton::Middle as u32);
+		const RIGHT_BUTTON = button_mask_to_value(sdl2::mouse::MouseButton::Right as u32);
+		const X1 = button_mask_to_value(sdl2::mouse::MouseButton::X1 as u32);
+		const X2 = button_mask_to_value(sdl2::mouse::MouseButton::X2 as u32);
 	}
 }
 
